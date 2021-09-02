9722e401a3b29838570e66c978ad073a8b8ffc59
zuyo <shield41791@gmail.com>
2021-09-03 00:04:43 +0900

    detail page : add image

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -7,9 +7,9 @@ import React from "react";
 
 class Detail extends React.Component {
     componentDidMount() {
-        const {location, history} = this.props;
+        const { location, history } = this.props;
         // 영화를 클릭하지 않고, 강제로 URL을 입력해서 간 경우 home으로 리다이렉트 
-        if(location.state === undefined) {
+        if (location.state === undefined) {
             history.push("/");
         }
         // console.log(location.state);
@@ -17,9 +17,16 @@ class Detail extends React.Component {
 
     // render()가 componentDidMount() 보다 먼저 실행되므로 여기도 예외처리 해야함.
     render() {
-        const {location} = this.props;
-        if(location.state) {
-            return <span>{location.state.title}</span>;
+        const { location } = this.props;
+        if (location.state) {
+            return (
+                <>
+                    <div>
+                        <span>{location.state.title}</span>
+                    </div>
+                    <img src={location.state.medium_cover_image} alt={location.state.title} />
+                </>
+            );
         } else {
             return null;
         }
